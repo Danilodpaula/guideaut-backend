e98cd4fdf599969a2c5d0084d16ac5519cbd94a3
danilo.paula <dsdp.eng22@uea.edu.br>
2025-11-04 14:07:22 -0400

    fix: unecessary crypto config removal

--- src/main/java/com/guideaut/project/config/CryptoConfig.java
+++ src/main/java/com/guideaut/project/config/CryptoConfig.java
@@ -1,14 +1,8 @@
 package com.guideaut.project.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class CryptoConfig {
-
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+  // vazio — o encoder já é definido em SecurityConfig
 }
