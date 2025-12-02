a38dfee65682431a685d9917305e8548e2ecb0fd
Guilherme Moraes <guighm@outlook.com.br>
2025-12-02 14:03:41 -0400

    Alteração de Componentes

--- src/main/java/com/guideaut/project/artefatos/dto/FormItemDto.java
+++ src/main/java/com/guideaut/project/artefatos/dto/FormItemDto.java
@@ -5,12 +5,14 @@ import com.guideaut.project.artefatos.model.FormItem;
 
 public record FormItemDto (
         String section,
+        Boolean isFixed,
         String question
 )
 {
     public FormItem toEntity(Form form) {
         FormItem item = new FormItem();
         item.setSection(this.section);
+        item.setIsFixed(this.isFixed);
         item.setQuestion(this.question);
         item.setForm(form);
         return item;
