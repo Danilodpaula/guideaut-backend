c8042c4ef8a40c8116dcec19b5c981f8dbc33afe
Danilo De Paula <120533663+Danilodpaula@users.noreply.github.com>
2025-12-02 22:11:33 -0300

    new cors origin added

--- src/main/java/com/guideaut/project/config/SecurityConfig.java
+++ src/main/java/com/guideaut/project/config/SecurityConfig.java
@@ -69,7 +69,8 @@ public class SecurityConfig {
         cfg.setAllowedOrigins(List.of(
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "http://localhost:3000"
+            "http://localhost:3000",
+            "https://guideaut.netlify.app"
         ));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
